8414e8fc05a6020157d35e8773155886fe837583
Emmanuel Gratuze <emmanuel.gratuze@gmail.com>
2020-11-08 20:15:12 +0100

    Clean home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Menu from 'components/Menu'
-// import { SpotifyContext } from 'components/SpotifyWrapper'
 import styles from './Home.module.css'
 
 const Home = () => {
-  // const { deviceId, token } = useContext(SpotifyContext);
   return (
     <div className={styles.container}>
       <Menu />
-
       Page d'accueil
     </div>
   )
