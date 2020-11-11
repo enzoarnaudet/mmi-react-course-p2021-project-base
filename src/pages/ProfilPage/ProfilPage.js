0c52ae3a1d14c333e0a549e68e174f341df89054
enzoarnaudet <56151607+enzoarnaudet@users.noreply.github.com>
2020-11-11 19:05:23 +0100

    profil

--- src/pages/ProfilPage/ProfilPage.js
+++ src/pages/ProfilPage/ProfilPage.js
@@ -5,31 +5,89 @@ import Page from '../../components/Page'
 const ProfilPage = () => {
     return (
         <Page>
-        <div div className="profil-page">
-            <h1>
-             Ton Profil
-            </h1>
-                <div>
-                    <img className={styles.logo} src={'discover.png'} />
-                </div>
-                <div>
-                    
-                </div>
-                <div>
-                    
-                </div>
-                <div>
-                    
-                </div>
 
-            <h1>
-             testesshZAOIHAZOI
-            </h1>
-        </div>
-        <div>
-        
-        </div>
+                 <div>
+                    <h1 className={styles.h1}>
+                    Ton Profil
+                    </h1>
+                 </div>
+    <div className={styles.section}>
+             <div className={styles.contenuleft}> 
+
+             </div>
+
+                
+             <div className={styles.contenumiddle}>
+             <div className={styles.contenumiddleleft}>
+                <div>
+                  <img className={styles.profil} src={'flyzen.png'} />
+                  </div> 
+                  <div>
+                  <h3 className={styles.h3}>13</h3>
+                   <h3 className={styles.h3}>      tracks</h3>
+
+                   <h3 className={styles.h2}>4243</h3>
+                   <h3 className={styles.h3}>      followers</h3>
+                 </div>
+                    <div>
+                        <p>Je suis un beatmaker français 
+provenant de bordeaux.
+Ma passion c’est la musique ainsi que le rap.
+
+Je fais différents genre de rap, de la drill, du two step, et de l’emo rap.
+
+</p>
+                    </div>
+             </div>
+             
+                        <div className={styles.contenumiddleright}>
+
+                            <div>
+                                <h1 className={styles.h1}>FLYZEN Beats</h1>
+                            </div>
+                             
+                             <div>
+                                <ul>
+                                    <li className={styles.puces}>
+                                        Titres
+                                    </li>
+                                    <li className={styles.puces}>
+                                        Albums
+                                    </li>
+                                    <li className={styles.puces}>
+                                        Playlists
+                                    </li>
+                                    <li className={styles.puces}>
+                                        Partages
+                                    </li>
+                                </ul>
+
+                             </div>
+                                             <div>
+                                                 <img className={styles.logo} src={'/cover/dystopia.png'} />
+                                                  Dystopia - Flyzen Beats 
+                                            </div>
+                                            <div>
+                                                 <img className={styles.logo} src={'/cover/dystopia.png'} />
+                                                  Dystopia - Flyzen Beats 
+                                            </div>
+                                            <div>
+                                                 <img className={styles.logo} src={'/cover/dystopia.png'} />
+                                                  Dystopia - Flyzen Beats 
+                                            </div>
+                                            <div>
+                                                 <img className={styles.logo} src={'/cover/dystopia.png'} />
+                                                  Dystopia - Flyzen Beats 
+                                            </div>
+
+                        </div>
+             </div>
+             <div className={styles.contenuright}> 
+
+             </div>
+    </div>
         </Page>
+        
         
     )
   }
