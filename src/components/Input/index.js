6c53dca75dddb9d360d6072c45f324947665761c
enzoarnaudet <56151607+enzoarnaudet@users.noreply.github.com>
2020-11-11 12:01:35 +0100

    icons

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -10,15 +10,21 @@ import styles from './Input.module.css'
   }
 */
 const Input = (props) => {
- const { type, placeholder, dark } = props
+ const { type, placeholder, dark, white } = props
   let className=styles.input
+  let classWhite=styles.input2
+ 
   if(dark) {
 className=styles.inputDark + ' ' + className
+  }
+
+  if(white) {
+    classWhite=styles.inputwhite
   }
   return (
     <div>
       <input
-   
+   classWhite={classWhite}
       className={className}
     
       placeholder={placeholder}
@@ -31,10 +37,14 @@ className=styles.inputDark + ' ' + className
 Input.propTypes = {
   type: PropTypes.oneOf(['password', 'email', 'text']),
   placeholder: PropTypes.string,
-  dark: PropTypes.bool
+  dark: PropTypes.bool,
+  white:PropTypes.bool
 }
 Input.defaultProps = {
   dark: true
+}
+Input.defaultProps = {
+  white: true
 }
 
 export default Input
