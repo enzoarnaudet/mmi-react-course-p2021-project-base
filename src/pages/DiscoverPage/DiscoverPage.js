7ca412681d1b4c18db1ca103e44a16073ed0ba1e
enzoarnaudet <56151607+enzoarnaudet@users.noreply.github.com>
2020-11-11 21:29:33 +0100

    css et mise en plage de la page discover

--- src/pages/DiscoverPage/DiscoverPage.js
+++ src/pages/DiscoverPage/DiscoverPage.js
@@ -6,63 +6,124 @@ import styles from './DiscoverPage.module.css'
 const DiscoverPage = () => {
   return (
     <Page>
-      <h1> Nouveautés</h1>
+      <h1 className={styles.h1}> Nouveautés</h1>
       <div className={styles.page}>
         
        
         <div>
           <img className={styles.logo} src={'/cover/dystopia.png'} />
-          Dystopia - Flyzen Beats 
+          <p className={styles.p}>Dystopia - Flyzen Beats </p>
         </div>
 
         <div>
           <img className={styles.logo} src={'/cover/introspection.png'} />
-          Introspection - Flyzen Beats
+         <p className={styles.p}>Introspection - Flyzen Beats</p> 
         </div>
 
           <div>
            <img className={styles.logo} src={'/cover/sku.png'} />
-           Sku - Travis Scott
+           <p className={styles.p}>Sku - Travis Scott</p> 
           </div>
 
           <div>
             <img className={styles.logo} src={'/cover/pablo.png'} />
-            Sad! - xxxtentacion
+            <p className={styles.p}>Sad! - xxxtentacion</p> 
           </div>
 
           <div>
              <img className={styles.logo} src={'/cover/sad.png'} />
-             Sad! - xxxtentacion
+             <p className={styles.p}>Sad! - xxxtentacion</p>   
           </div>
           </div>
           <div className={styles.page2}>
 
           <div>
             <img className={styles.logo} src={'/cover/mexico.png'} />
-            Mexico - DA UZI
+            <p className={styles.p}>Mexico - DA UZI</p>  
           </div>
 
           <div>
-            <img className={styles.logo} src={'/cover/dystopia.png'} />
-            Lounge - Menz
+            <img className={styles.logo} src={'/cover/lounge.png'} />
+            <p className={styles.p}>Lounge - Menz</p> 
           </div>
 
           <div>
             <img className={styles.logo} src={'/cover/mansland.png'} />
-            MAN'S LAND - And the beast
+            <p className={styles.p}>MAN'S LAND - And the beast</p> 
           </div>
 
           <div>
-            <img className={styles.logo} src={'/cover/dystopia.png'} />
-            Apocalypse - Menz
+            <img className={styles.logo} src={'/cover/apocalypse.png'} />
+            <p className={styles.p}>Apocalypse - Menz</p>  
           </div>
 
           <div>
             <img className={styles.logo} src={'/cover/rage.png'} />
-            Turbo S - 7jaws
+            <p className={styles.p}>Turbo S - 7jaws</p> 
           </div>
       </div>
-      <h1>Genres</h1>
+
+
+
+
+      <h1 className={styles.h1}>Genres</h1>
+
+
+
+      <div className={styles.page}>
+    
+        <div>
+          <img className={styles.logo} src={'/cover/drill.jpg'} />
+          <p className={styles.p}>DRILL</p>
+        </div>
+
+        <div>
+          <img className={styles.logo} src={'/cover/drill.jpg'} />
+         <p className={styles.p}>EMO RAP</p> 
+        </div>
+
+          <div>
+           <img className={styles.logo} src={'/cover/drill.jpg'} />
+           <p className={styles.p}>TWO-STEP</p> 
+          </div>
+
+          <div>
+            <img className={styles.logo} src={'/cover/drill.jpg'} />
+            <p className={styles.p}>RAP METAL</p> 
+          </div>
+
+          <div>
+             <img className={styles.logo} src={'/cover/drill.jpg'} />
+             <p className={styles.p}>RAPCORE</p>   
+          </div>
+          </div>
+          <div className={styles.page2}>
+
+          <div>
+            <img className={styles.logo} src={'/cover/drill.jpg'} />
+            <p className={styles.p}>POP RAP</p>  
+          </div>
+
+          <div>
+            <img className={styles.logo} src={'/cover/drill.jpg'} />
+            <p className={styles.p}>COUNTRY RAP</p> 
+          </div>
+
+          <div>
+            <img className={styles.logo} src={'/cover/drill.jpg'} />
+            <p className={styles.p}>CRUNK</p> 
+          </div>
+
+          <div>
+            <img className={styles.logo} src={'/cover/drill.jpg'} />
+            <p className={styles.p}>CLOUD RAP</p>  
+          </div>
+
+          <div>
+            <img className={styles.logo} src={'/cover/drill.jpg'} />
+            <p className={styles.p}>GANGSTA RAP</p> 
+          </div>
+      </div>
     </Page>
   )
 }
